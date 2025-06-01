997bb9c68df760578bf7bb54382d209f68a79e19
Petar Petrov <petizad55@gmail.com>
2025-06-01 15:45:45 +0200

    dvc: commit experiment 44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a

--- training/train_model.py
+++ training/train_model.py
@@ -1,19 +1,24 @@
 """Train model(s)"""
 
 import joblib
-
+from dvclive import Live
 from sklearn.naive_bayes import GaussianNB
 
 
 def train():
     """Run training step"""
-    x = joblib.load('output/splits/X_train.jbl')
-    y = joblib.load('output/splits/y_train.jbl')
+    with Live() as live:
+        live.log_param("classifier", "GaussianNB")
+        
+        x = joblib.load('output/splits/X_train.jbl')
+        y = joblib.load('output/splits/y_train.jbl')
 
-    classifier = GaussianNB()
-    classifier.fit(x, y)
+        classifier = GaussianNB()
+        classifier.fit(x, y)
 
-    joblib.dump(classifier, 'output/model.jbl')
+        joblib.dump(classifier, 'output/model.jbl')
+
+        live.log_artifact("output/model.jbl", type="model")
 
 
 if __name__ == "__main__":
