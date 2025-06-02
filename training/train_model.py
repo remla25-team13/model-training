205ccfb7977fd07373314593aae921da3c538e5b
Petar Petrov <petizad55@gmail.com>
2025-06-02 22:18:31 +0200

    fix pytest

--- training/train_model.py
+++ training/train_model.py
@@ -5,26 +5,27 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from dvclive import Live
 
 
-def train():
+def train(live=None):
     """Run training step"""
-    with Live() as live:
+    if live:
         live.log_param("classifiers", "GaussianNB, BernouliNB")
 
-        x = joblib.load('output/splits/X_train.jbl')
-        y = joblib.load('output/splits/y_train.jbl')
+    x = joblib.load('output/splits/X_train.jbl')
+    y = joblib.load('output/splits/y_train.jbl')
 
-        gauss_classifier = GaussianNB()
-        multi_classifier = MultinomialNB()
+    gauss_classifier = GaussianNB()
+    multi_classifier = MultinomialNB()
+    gauss_classifier.fit(x, y)
+    multi_classifier.fit(x, y)
 
-        gauss_classifier.fit(x, y)
-        multi_classifier.fit(x, y)
+    joblib.dump(gauss_classifier, 'output/model-gauss.jbl')
+    joblib.dump(multi_classifier, 'output/model-multi.jbl')
 
-        joblib.dump(gauss_classifier, 'output/model-gauss.jbl')
-        joblib.dump(multi_classifier, 'output/model-multi.jbl')
-
-        live.log_artifact("output/model.jbl", type="model")
+    if live:
+        live.log_artifact("output/model-gauss.jbl", type="model")
         live.log_artifact("output/model-multi.jbl", type="model")
 
 
 if __name__ == "__main__":
-    train()
+    with Live() as live:
+        train(live)
