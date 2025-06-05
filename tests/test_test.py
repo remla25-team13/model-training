4f6833ad5846041a726abecd9280d71568f5e0ef
“Tijmen <tgmeijer@student.tudelft.nl>
2025-06-06 00:14:31 +0200

    remove test file

--- tests/test_test.py
+++
@@ -1,9 +0,0 @@
-# test_checks.py
-from sklearn.linear_model import LogisticRegression
-
-model = LogisticRegression()
-X_train = [[1, 2], [3, 4]]
-y_train = [0, 1]
-
-model.fit(X_train)           # ⚠ Should trigger "fit-missing-y"
-preds = model.predict(X_train)  # ⚠ Should trigger "predict-on-training-data"
